44c9f0e24b54dbd6a85cad8c09f67202c58acfbc
dotiendiep <dotiendiep@gmail.com>
2012-02-28 15:12:37 +0000

    Xóa header tệp tin... Chuẩn bị chuẩn hóa nội dung

--- upload/languages/vi_vn/admin/ebao_commend.php
+++ upload/languages/vi_vn/admin/ebao_commend.php
@@ -1,18 +1,4 @@
 <?php
-
-/**
- * ECSHOP 易宝语言文件
- * ============================================================================
- * 版权所有 2005-2011 上海商派网络科技有限公司，并保留所有权利。
- * 网站地址: http://www.ecshop.com；
- * ----------------------------------------------------------------------------
- * 这不是一个自由软件！您只能在不用于商业目的的前提下对程序代码进行修改和
- * 使用；不允许对程序代码以任何形式任何目的的再发布。
- * ============================================================================
- * $Author: liubo $
- * $Id: ebao_commend.php 17217 2011-01-19 06:29:08Z liubo $
-*/
-
 $_LANG['select_method'] = 'Ways to choose merchandise:';
 $_LANG['by_cat'] = 'From the classification, under the choice of brand merchandise';
 $_LANG['by_sn'] = 'Classification, under the brand names of all merchandise';
