38efc61dd71cecd0c25e45b8f422ccede4e1f641
dotiendiep@gmail.com <dotiendiep@gmail.com@421f3098-7936-7369-074a-85a070371dde>
2012-03-05 07:56:43 +0000

    Xóa bỏ phần check kích hoạt phần mềm

--- trunk/upload/install/templates/active_content.php
+++ trunk/upload/install/templates/active_content.php
@@ -1,4 +1,5 @@
 <br>
-<div><iframe id="frm1" src="http://passport.shopex.cn/index.php?ctl=iframeent&act=index&url=<?php echo $link_url;?>&t=<?php echo $nowtime;?>" frameborder="0" allowtransparency='true' scrolling='auto' width='666' height='410'></iframe>
+<div>
+<!--Nothing Here Now-->
 </div><br>
-<a href="<?php echo $admin_url;?>"><b>跳过激活流程，立即进入管理后台</b></a>
+<a href="<?php echo $admin_url;?>"><b>Bỏ qua phần kích hoạt, tới trang quản trị</b></a>
